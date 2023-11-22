d58ed53ecc72cc9a35896dc1d5e0e2b3ab1cba2d
OliviaTetteh <olivet36@yahoo.com>
2023-11-22 10:56:47 +0000

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     num = 0
         try:
             while num is not x:
-                print(my_list[i], end="")
+                print(my_list[num], end='')
                 num += 1
         except IndexError:
             None
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
-
+#!/usr/bin/python3i
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
