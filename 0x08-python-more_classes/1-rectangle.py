666b3da894fdf810eeb1d5837946a3cc24ff89a0
OliviaTetteh <olivet36@yahoo.com>
2023-11-30 13:29:18 +0000

    python more classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError
+            raise ValueError("height must be >= 0")
         self.__height = value
--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -5,9 +5,9 @@ number of instances = 0
 print_symbol = "#"
 
 def __init__(self, width=0, height=0):
-    type(self).number_of_instances +=1
     self.width = width
     self.height = height
+    Rectangle.number_of_instances += 1
 
 @property
 def width(self):
@@ -19,3 +19,52 @@ def width(self, value):
         raise TypeError
     if value < 0:
         raise ValueError
+     self.__width = value
+
+    @property
+    def height(self):
+        
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self) -> str:
+        
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        rectangle = ""
+        for column in range(self.__height):
+            for row in range(self.__width):
+                try:
+                    rectangle += str(self.print_symbol)
+                except Exception:
+                    rectangle += type(self).print_symbol
+            if column < self.__height - 1:
+                rectangle += "\n"
+        return (rectangle)
+
+    def __repr__(self):
+        
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+    def __del__(self):
+        
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
