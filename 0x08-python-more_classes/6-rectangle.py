1fb018b452258fff6be92b9cf405cc1135f7da65
OliviaTetteh <olivet36@yahoo.com>
2023-11-30 14:51:26 +0000

    0x08-python-more_classes

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python3
-class Rectangle:
+"""A class that defines a rectangle"""
 
-    number of instances = 0
+
+class Rectangle:
+    """this represents a rectangle"""
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
+        """Initializing this rectangle class
+        Args:
+            width: represents the width of the rectangle
+            height: represents the height of the rectangle
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
 
     @property
     def width(self):
+        """retrieves width attribute"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """sets width attribute"""
         if not isinstance(value, int):
-            raise TypeError
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -59,8 +72,3 @@ class Rectangle:
     def __repr__(self):
         """returns a string representation of the rectangle"""
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
-
-    def __del__(self):
-        """prints a message for every object that is deleted"""
-        print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
